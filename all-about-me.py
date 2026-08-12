673ab64ab422d5c6b4603b43acde00bc246aab63
Dev-binabdallah <bin.abdallah.jr@gmail.com>
2026-08-12 16:58:19 +0300

    Add personal information Python program

---
+++ all-about-me.py
@@ -0,0 +1,12 @@
+print ("Hello World")
+First_Name = "ABDULLAHI"
+Second_Name = "ABDI"
+Last_Name = "MOHAMED"
+Age = 26
+Favourite_Language = "Kiswahili"
+Goal = "Programmer"
+print("My names are:", First_Name, Second_Name, Last_Name)
+print("Age:", Age)
+print("Favourite language:", Favourite_Language)
+print("My goal is to become:", Goal)
+#Hello there! This program displays my personal information and career goal.
