3c2dd1aa326f0a77100ed2a3776e694ddcae3401
Dev-binabdallah <bin.abdallah.jr@gmail.com>
2026-08-13 10:02:47 +0300

    Build simple Mad Libs story

---
+++ mad-libs-project.py
@@ -0,0 +1,10 @@
+name = input("Enter a name: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+
+story = (
+    f"Once upon a time, {name} lived in a {adjective} village in {place}. "
+    f"One day, {name} found a {noun}, {verb} the {noun} and took it home.")
+print(story)
